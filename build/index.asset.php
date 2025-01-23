241cc7be5ce21d189d320b019b90953cdb41ad1d
Sakib <nazmunsakib81@gmail.com>
2025-01-23 23:18:31 +0600

    Include add wp script

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,1 @@
-<?php 
-return array(
-    'dependencies' => array('react', 'wp-dom-ready', 'wp-element'), 
-    'version' => '50d597fb3842894015e7'
-);
+<?php return array('dependencies' => array('react', 'wp-dom-ready', 'wp-element'), 'version' => '50d597fb3842894015e7');
